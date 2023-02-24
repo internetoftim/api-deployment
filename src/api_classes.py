e787c8073c5ef0b970381efbb16d205b12aff8e3
Tim Santos <elsantos00@gmail.com>
2023-02-24 17:22:34 +0000

    sd

--- src/api_classes.py
+++ src/api_classes.py
@@ -11,6 +11,13 @@ class QA(BaseModel):
     context: str
     question: str
 
+# Stable Diffusion
+class StableDiffusionRequest(BaseModel):
+    prompt: str = "Big red dog"
+    random_seed: int = int(time.time())
+    guidance_scale: float = 7.5
+    return_json: bool = False    
+
 class StableDiffusion2Request(StableDiffusionRequest):
     negative_prompt: str = None
     guidance_scale: float = 9
